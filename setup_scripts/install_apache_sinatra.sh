49b8e4d6ccd0c0d5f937252d7980c6f55c92b6d2
Chris Kairalla <chris@mailtochris.com>
2013-03-20 22:36:16 -0400

    added apache and sinatra script

---
+++ setup_scripts/install_apache_sinatra.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#install http server
+apt-get -y install apache2
+wget http://www.itp-redial.com/class/wp-content/uploads/2012/03/httpd.conf_.txt -O /etc/apache2/httpd.conf
+#cp /usr/share/doc/apache2.2-common/examples/apache2/extra/httpd-userdir.conf /etc/apache2/httpd.conf
+a2enmod userdir
+service apache2 restart
+
+#install sinatra
+gem install sinatra
+gem install haml
+
+#install passenger so sinatra can be run in apache
+apt-get -y install libcurl4-openssl-dev
+apt-get -y install apache2-prefork-dev
+apt-get -y install libapr1-dev
+apt-get -y install libaprutil1-dev
+#apt-get -y install libapache2-mod-passenger
+gem install passenger
+passenger-install-apache2-module -a
+#sed -i 's/ 00:00:00.000000000Z//' /var/lib/gems/1.8/specifications/*
+echo "LoadModule passenger_module /usr/local/rvm/gems/ruby-1.9.3-p392/gems/passenger-3.0.19/ext/apache2/mod_passenger.so" >> /etc/apache2/httpd.conf
+echo "PassengerRoot /usr/local/rvm/gems/ruby-1.9.3-p392/gems/passenger-3.0.19" >> /etc/apache2/httpd.conf
+echo "PassengerRuby /usr/local/rvm/wrappers/ruby-1.9.3-p392/ruby" >> /etc/apache2/httpd.conf
+
+service apache2 restart
