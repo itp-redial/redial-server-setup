eb4d8263f0566db907904cdb160abd0132d975a6
itp-redial <ckairalla.mail@gmail.com>
2013-03-21 13:03:50 -0300

    Update install_mysql.sh
    removed libmysql-ruby

--- setup_scripts/install_mysql.sh
+++ setup_scripts/install_mysql.sh
@@ -1,4 +1,4 @@
 #/usr/bin/env bash
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get -y install mysql-server mysql-client libmysql-ruby libmysqlclient-dev
+apt-get -y install mysql-server mysql-client libmysqlclient-dev
