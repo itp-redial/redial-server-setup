523af8e3324af62ad98c12a2abb306a3a4e83f4b
itp-redial <ckairalla.mail@gmail.com>
2013-03-21 19:09:10 -0300

    New datamapper install.

---
+++ setup_scripts/install_datamapper.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [ -z `which mysql` ]; then 
+  echo "no mysql.  Attempting to run install_mysql.sh in this directory."
+  bash install_mysql.sh
+fi
+
+if [ -z `which mysql` ]; then 
+  echo "Still can't find mysql.  Please locate and run install_mysql.sh and then rerun this script."
+  exit 0
+fi
+
+gem install datamapper
+gem install dm-mysql-adapter
