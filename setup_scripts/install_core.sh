0e2c6d7ab8c799764f227d446a7b39e38a8ad813
Chris Kairalla <chris@mailtochris.com>
2013-03-20 22:21:51 -0400

    added core and ruby setup scripts

---
+++ setup_scripts/install_core.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get -y upgrade
+apt-get -y install build-essential wget libssl-dev libncurses5-dev libnewt-dev libxml2-dev linux-headers-$(uname -r)
+apt-get -y install git-core subversion
+
+#set timezone
+ln -sf /usr/share/zoneinfo/US/Eastern /etc/localtime
+#set default shell to bash
+sed -e 's/SHELL=\/bin\/sh/SHELL=\/bin\/bash/g' -i /etc/default/useradd
+
+#copy public IP address to file /etc/publicIP
+export PUBLIC_IP=`curl http://checkip.dyndns.org/ | grep -o  "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*"`
+echo $PUBLIC_IP > /etc/publicIP
+#install scripts from git
+cd /root
+git clone https://github.com/itp-redial/redial-server-setup.git
+echo "Rebooting your computer... Please wait 5 minutes for reboot to complete" 
+sleep 5
+reboot
---
+++ setup_scripts/install_ruby.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#get yaml libs
+apt-get -y install libyaml-dev
+#install ruby rvm
+\curl -L https://get.rvm.io | sudo bash -s stable
+source /etc/profile.d/rvm.sh
+#install necessary packages
+rvm pkg install readline
+rvm pkg install openssl
+rvm pkg install iconv
+rvm install 1.9.3-p392
+rvm rubygems current
+rvm use 1.9.3 --default
+# create symbolic link to /usr/bin/ruby for legacy compatibility
+ln -s `which ruby` /usr/bin/ruby
