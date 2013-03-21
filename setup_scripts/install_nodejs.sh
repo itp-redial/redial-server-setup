592a4dc7081aa8c1f8a89f88f1cc63985222e59b
itp-redial <ckairalla.mail@gmail.com>
2013-03-21 14:39:15 -0300

    completed transition back to 0.8.x

--- setup_scripts/install_nodejs.sh
+++ setup_scripts/install_nodejs.sh
@@ -7,8 +7,8 @@ apt-get -y install python-software-properties python g++ make
 yes | add-apt-repository ppa:chris-lea/node.js-legacy
 apt-get update
 apt-get -y install nodejs
-#apt-get -y install npm
-#apt-get -y install nodejs-dev
+apt-get -y install npm
+apt-get -y install nodejs-dev
 export NODE_PATH=/usr/lib/node_modules/
 echo "export NODE_PATH=$NODE_PATH" >> ~/.bashrc
 npm cache clear
