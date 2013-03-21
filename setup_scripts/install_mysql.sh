256be1efdd4937cc17c38ee54104a331c5540e30
itp-redial <ckairalla.mail@gmail.com>
2013-03-21 13:11:47 -0300

    Update install_mysql.sh
    added password

--- setup_scripts/install_mysql.sh
+++ setup_scripts/install_mysql.sh
@@ -2,3 +2,10 @@
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get -y install mysql-server mysql-client libmysqlclient-dev
+echo ""
+echo "Please choose a password for MySQL root, or hit Enter for no password:
+read -p "New Password > " NEW_PASS
+if [ -n "$NEW_PASS" ]; then
+  mysqladmin -u root password $NEW_PASS
+fi
+
