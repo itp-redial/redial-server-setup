6a135cd7a58eb2e70ca69c0047ef8b0d0faa92cf
itp-redial <ckairalla.mail@gmail.com>
2013-03-25 12:43:44 -0300

    Update install_datamapper.sh

--- setup_scripts/install_datamapper.sh
+++ setup_scripts/install_datamapper.sh
@@ -9,6 +9,8 @@ if [ -z `which mysql` ]; then
   echo "Still can't find mysql.  Please locate and run install_mysql.sh and then rerun this script."
   exit 0
 fi
+#just in case this is run right after RVM install...
+source /etc/profile.d/rvm.sh
 
 gem install datamapper
 gem install dm-mysql-adapter
