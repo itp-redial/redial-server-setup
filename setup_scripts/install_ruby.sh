f9d37c08212c0f5d00ef6fae079fd7ecf324dc50
itp-redial <ckairalla.mail@gmail.com>
2013-03-26 19:16:27 -0300

    added autolibs flag to rvm script

--- setup_scripts/install_ruby.sh
+++ setup_scripts/install_ruby.sh
@@ -3,12 +3,8 @@
 #get yaml libs
 apt-get -y install libyaml-dev
 #install ruby rvm
-\curl -L https://get.rvm.io | sudo bash -s stable
+\curl -L https://get.rvm.io | sudo bash -s stable --autolibs=enabled
 source /etc/profile.d/rvm.sh
-#install necessary packages
-rvm pkg install readline
-rvm pkg install openssl
-rvm pkg install iconv
 rvm install 1.9.3-p392
 rvm rubygems current
 rvm use 1.9.3 --default
