8dedcb7bd0e634e85be94a2d56233ca15ed854e1
Chris Kairalla <chris@mailtochris.com>
2014-07-29 17:11:10 -0400

    changed Ruby to 2.0.0

--- setup_scripts/install_ruby.sh
+++ setup_scripts/install_ruby.sh
@@ -5,9 +5,9 @@ apt-get -y install libyaml-dev
 #install ruby rvm
 \curl -L https://get.rvm.io | sudo bash -s stable --autolibs=enabled
 source /etc/profile.d/rvm.sh
-rvm install 1.9.3-p392
+rvm install 2.0.0-p481
 rvm rubygems current
-rvm use 1.9.3 --default
+rvm use 2.0.0 --default
 # create symbolic link to /usr/bin/ruby for legacy compatibility
 ln -s `which ruby` /usr/bin/ruby
 source /etc/profile.d/rvm.sh
