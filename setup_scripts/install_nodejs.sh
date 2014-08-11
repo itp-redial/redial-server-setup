99f31733a37f707ae2d8639f2b91aba1e063bb72
Chris Kairalla <chris@mailtochris.com>
2014-08-10 21:28:14 -0400

    Update install_nodejs.sh
    removed unnecessary package installers

--- setup_scripts/install_nodejs.sh
+++ setup_scripts/install_nodejs.sh
@@ -2,13 +2,13 @@
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get -y install python-software-properties python g++ make
-#yes | add-apt-repository ppa:chris-lea/node.js
+yes | add-apt-repository ppa:chris-lea/node.js
 #install node 0.8.x, not 0.10.x
-yes | add-apt-repository ppa:chris-lea/node.js-legacy
+#yes | add-apt-repository ppa:chris-lea/node.js-legacy
 apt-get update
 apt-get -y install nodejs
-apt-get -y install npm
-apt-get -y install nodejs-dev
+#apt-get -y install npm
+#apt-get -y install nodejs-dev
 export NODE_PATH=/usr/lib/node_modules/
 echo "export NODE_PATH=$NODE_PATH" >> ~/.bashrc
 npm cache clear
